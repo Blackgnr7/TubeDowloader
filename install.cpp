de6fdc41977639c8805ecb42168b215c66689155
IyinG <165347923+Blackgnr7@users.noreply.github.com>
2025-10-05 11:36:24 -0300

    3.0

--- install.cpp
+++ install.cpp
@@ -12,17 +12,9 @@ int main(int argc, char* argv[]){
         std::string command = "setx PATH \"%PATH%;" + diretorio + "\"";       
         system(command.c_str());
     #elif __linux__
-        std::string path = argv[0];;
-        size_t last_slash_pos = path.find_last_of("/\\");
-        std::string diretorio = path.substr(0, last_slash_pos);
-        std::cout << diretorio << std::endl;
-        system("sudo mv" + diretorio + "/usr/local/bin")
+        system("sudo mv tubedowloader /usr/local/bin")
     #elif __APPLE__
-        std::string path = argv[0];;
-        size_t last_slash_pos = path.find_last_of("/\\");
-        std::string diretorio = path.substr(0, last_slash_pos);
-        std::cout << diretorio << std::endl;
-        system("sudo mv" + diretorio + "/usr/local/bin")
+        system("sudo mv tubedowloader /usr/local/bin")
     #else
         printf("Desconhecido\n");
     #endif
